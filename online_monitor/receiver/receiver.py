dbf2a2bac3d7a1a3d0db2843be59ecedecec297a
DavidLP <pohl@physik.uni-bonn.de>
2015-10-02 17:09:58 +0200

    BUG: fix thread close ENH: use device name MAINT: better info output

--- online_monitor/receiver/receiver.py
+++ online_monitor/receiver/receiver.py
@@ -1,5 +1,6 @@
 from PyQt4 import Qt
 from pyqtgraph.Qt import QtCore
+from PyQt4.QtCore import pyqtSlot, pyqtSignal
 import zmq
 import logging
 from threading import Event
@@ -8,25 +9,30 @@ from online_monitor import utils
 
 
 class DataWorker(QtCore.QObject):
-    def __init__(self):
+    data = QtCore.pyqtSignal(dict)
+    finished = QtCore.pyqtSignal()
+
+    def __init__(self, deserialzer):
         QtCore.QObject.__init__(self)
-        print 'Init DATA WORKER'
+        self.deserialzer = deserialzer
         self._stop_readout = Event()
 
     def connect(self, receive_address):
+        print receive_address
         self.context = zmq.Context()
         self.receiver = self.context.socket(zmq.SUB)  # subscriber
         self.receiver.setsockopt(zmq.SUBSCRIBE, '')  # do not filter any data
         self.receiver.connect(receive_address)
 
     def receive_data(self):  # infinite loop via QObject.moveToThread(), does not block event loop
-        while(not self._stop_readout.wait(1.01)):  # use wait(), do not block here
-            print 'RECEIVE'
+        while(not self._stop_readout.wait(0.01)):  # use wait(), do not block here
             try:
-                data = self.receiver.recv(flags=zmq.NOBLOCK)
-                print data
+                data_serialized = self.receiver.recv(flags=zmq.NOBLOCK)
+                data = self.deserialzer(data_serialized)
+                self.data.emit(data)
             except zmq.Again:
                 pass
+        self.finished.emit()
 
     def shutdown(self):
         self._stop_readout.set()
@@ -39,35 +45,46 @@ class Receiver(QtCore.QObject):
 
     Usage:
     '''
-
-    def __init__(self, receive_address, data_type, max_cpu_load=100, loglevel='INFO'):
+    def __init__(self, receive_address, data_type, device='Undefined', max_cpu_load=100, loglevel='INFO'):
+        QtCore.QObject.__init__(self)
         self.data_type = data_type
         self.receive_address = receive_address
         self.max_cpu_load = max_cpu_load
+        self.device = device  # name of the DAQ/device
 
         utils.setup_logging(loglevel)
-
-        logging.debug("Initialize %s receiver for data at %s", self.data_type, self.receive_address)
-
+        logging.debug("Initialize %s receiver for device %s at %s", self.data_type, self.device, self.receive_address)
         self.setup_receiver_device()
 
     def setup_receiver_device(self):  # start new receiver thread
+        logging.info("Start %s receiver for device %s at %s", self.data_type, self.device, self.receive_address)
         self.thread = QtCore.QThread()  # no parent
-        self.worker = DataWorker()  # no parent
-        self.worker.moveToThread(self.thread)
+
+        self.worker = DataWorker(self.deserialze_data)  # no parent
+        self.worker.moveToThread(self.thread)  # move worker instance to new thread
         self.worker.connect(self.receive_address)  # connect to ZMQ publisher
-#         self.aboutToQuit.connect(self.worker.stop)  # QtGui.QApplication
-        self.thread.started.connect(self.worker.receive_data)
-        
-#         self.thread.started.connect(self.worker.receive_data)
-        
-#         self.worker.finished.connect(self.thread.quit)
-#         self.worker.finished.connect(self.worker.deleteLater)
-#         self.thread.finished.connect(self.thread.deleteLater)
-#         self.thread.start()
+        self.worker.finished.connect(self.thread.quit)  # quit thread on worker finished
+        self.worker.data.connect(self.handle_data)
+
+        self.thread.started.connect(self.worker.receive_data)  # start receive data loop when thread starts
+        self.thread.finished.connect(self.finished_info)  # print on thread finished info
+        self.thread.start()  # start thread
 
     def shutdown(self):
-        self.exit.set()
+        self.worker.shutdown()  # set signal to quit receive loop; can take some time
+        self.thread.exit()  # tell thread to exit, loop is/should be terminated already
+        self.thread.wait(500)  # delay needed if thread di not exit yet, otherwise message: QThread: Destroyed while thread is still running
+
+    def finished_info(self):  # called when thread finished successfully
+        logging.info("Close %s receiver for device %s at %s", self.data_type, self.device, self.receive_address)
 
     def setup_plots(self, parent):
         raise NotImplementedError("You have to implement a setup_plots method!")
+
+    def handle_data(self, data):
+        ''' Handle data gets a dictionary with data and sets the visualization accordningly'''
+        raise NotImplementedError("You have to implement a handle_data method!")
+
+    def deserialze_data(self, data):
+        ''' Has to convert the data do a python dict '''
+        raise NotImplementedError("You have to implement a deserialze_data method. Look at the examples!")
