66a6cc0f6c7ae1ece15d393047fb6a11e1548cbb
DavidLP <pohl@physik.uni-bonn.de>
2015-10-02 17:10:26 +0200

    MAINT

--- online_monitor/start_converter.py
+++ online_monitor/start_converter.py
@@ -3,8 +3,8 @@ from converter.converter_manager import ConverterManager
 
 
 if __name__ == '__main__':
-#     args = utils.parse_arguments()
-#     utils.setup_logging(args.log)
+    args = utils.parse_arguments()
+    utils.setup_logging(args.log)
 
-    cm = ConverterManager('configuration.yaml')#args.config_file)
+    cm = ConverterManager(args.config_file)
     cm.start()  # blocking function, returns on SIGTERM signal
