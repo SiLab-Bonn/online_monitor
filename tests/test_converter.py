dc8739be7752201b8c888f6ba90ef2d56e49d375
davidlp <pohl@physik.uni-bonn.de>
2015-10-05 22:58:54 +0200

    ENH: fix test case

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -20,7 +20,7 @@ def create_config_yaml(n_converter):
     for index in range(n_converter):
         devices['DUT%s' % index] = {
             'data_type': 'forwarder',
-            'receive_address': 'tcp://127.0.0.1:56%02d' % index,
+            'receive_address': 'tcp://127.0.0.1:55%02d' % index,
             'send_address': 'tcp://127.0.0.1:55%02d' % (index + 1)
         }
     conf['converter'] = devices
@@ -49,19 +49,27 @@ class TestConverter(unittest.TestCase):
     def test_converter_communication(self):  # start 10 forwarder in a chain a do "whisper down the lane"
         n_python = sum(1 for i in psutil.process_iter() if 'python' in i.name())  # number of python instances before multi threading
         converter_manager_process = subprocess.Popen(["python", r"../online_monitor/start_converter.py", 'tmp_cfg_10_converter.yml'], shell=True, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
-
+        time.sleep(1)
+        no_data = True
         context = zmq.Context()
         # Socket facing last converter
         sender = context.socket(zmq.PUB)
-        sender.bind(r'tcp://127.0.0.1:5600')
+        sender.bind(r'tcp://127.0.0.1:5500')
         receiver = context.socket(zmq.SUB)  # subscriber
         receiver.connect(r'tcp://127.0.0.1:5502')
         receiver.setsockopt(zmq.SUBSCRIBE, '')  # do not filter any data
-        time.sleep(2)
-        sender.send_json("Test")
-        time.sleep(1)
-        test = receiver.recv_json()
-        print test
+        time.sleep(1.1)
+        msg = 'This is a test message'
+        sender.send_json(msg)
+        time.sleep(1.1)
+        try:
+            ret_msg = receiver.recv_json(flags=zmq.NOBLOCK)
+            no_data = False
+            self.assertEqual(msg, ret_msg)
+        except zmq.Again:
+            pass
+
+        self.assertFalse(no_data, 'Did not receive any data') 
 #         os.kill(converter_manager_process.pid, signal.SIGTERM)
         kill(converter_manager_process.pid)
         n_python_2 = sum(1 for i in psutil.process_iter() if 'python' in i.name())
