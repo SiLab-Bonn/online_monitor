cc6df9eb60bbbc32c486c50bf5bafb6a7417bf73
DavidLP <pohl@physik.uni-bonn.de>
2015-10-06 11:52:53 +0200

    ENH: correlator device defined

---
+++ online_monitor/converter/correlator.py
@@ -0,0 +1,15 @@
+from transceiver import Transceiver
+
+
+class Correlator(Transceiver):
+    def __init__(self, *args, **kwargs):
+        Transceiver.__init__(self, *args, **kwargs)
+        if self.n_receivers < 2:
+            raise ValueError('A correlator needs at least two receivers! Specify the receive adresses in the config file.')
+
+    def setup_interpretation(self):
+        pass
+
+    def interpret_data(self, data):
+        print data
+        return data
