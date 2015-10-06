d59e7d78fe17bd61b849c3c25813f26828c3eb64
DavidLP <pohl@physik.uni-bonn.de>
2015-10-06 13:53:59 +0200

    ENH/API: activate receiver only if the receiver tab widget is viewed MAINT: device --> name

--- online_monitor/receiver/receiver.py
+++ online_monitor/receiver/receiver.py
@@ -1,6 +1,4 @@
-from PyQt4 import Qt
 from pyqtgraph.Qt import QtCore
-from PyQt4.QtCore import pyqtSlot, pyqtSignal
 import zmq
 import logging
 from threading import Event
@@ -18,7 +16,6 @@ class DataWorker(QtCore.QObject):
         self._stop_readout = Event()
 
     def connect(self, receive_address):
-        print receive_address
         self.context = zmq.Context()
         self.receiver = self.context.socket(zmq.SUB)  # subscriber
         self.receiver.setsockopt(zmq.SUBSCRIBE, '')  # do not filter any data
@@ -45,44 +42,53 @@ class Receiver(QtCore.QObject):
 
     Usage:
     '''
-    def __init__(self, receive_address, data_type, device='Undefined', max_cpu_load=100, loglevel='INFO'):
+    def __init__(self, receive_address, data_type, name='Undefined', max_cpu_load=100, loglevel='INFO'):
         QtCore.QObject.__init__(self)
         self.data_type = data_type
         self.receive_address = receive_address
         self.max_cpu_load = max_cpu_load
-        self.device = device  # name of the DAQ/device
+        self.name = name  # name of the DAQ/device
+        self._active = False  # flag to tell receiver if its active (viewed int the foreground)
 
         utils.setup_logging(loglevel)
-        logging.debug("Initialize %s receiver for device %s at %s", self.data_type, self.device, self.receive_address)
+        logging.debug("Initialize %s receiver %s at %s", self.data_type, self.name, self.receive_address)
         self.setup_receiver_device()
 
     def setup_receiver_device(self):  # start new receiver thread
-        logging.info("Start %s receiver for device %s at %s", self.data_type, self.device, self.receive_address)
+        logging.info("Start %s receiver %s at %s", self.data_type, self.name, self.receive_address)
         self.thread = QtCore.QThread()  # no parent
 
         self.worker = DataWorker(self.deserialze_data)  # no parent
         self.worker.moveToThread(self.thread)  # move worker instance to new thread
         self.worker.connect(self.receive_address)  # connect to ZMQ publisher
         self.worker.finished.connect(self.thread.quit)  # quit thread on worker finished
-        self.worker.data.connect(self.handle_data)
+        self.worker.data.connect(self.handle_data_if_active)
 
         self.thread.started.connect(self.worker.receive_data)  # start receive data loop when thread starts
         self.thread.finished.connect(self.finished_info)  # print on thread finished info
         self.thread.start()  # start thread
 
+    def active(self, value):  # slot called if the receiver tab widget gets active
+        self._active = value
+
     def shutdown(self):
         self.worker.shutdown()  # set signal to quit receive loop; can take some time
         self.thread.exit()  # tell thread to exit, loop is/should be terminated already
-        self.thread.wait(500)  # delay needed if thread di not exit yet, otherwise message: QThread: Destroyed while thread is still running
+        self.thread.wait(500)  # delay needed if thread did not exit yet, otherwise message: QThread: Destroyed while thread is still running
 
     def finished_info(self):  # called when thread finished successfully
-        logging.info("Close %s receiver for device %s at %s", self.data_type, self.device, self.receive_address)
+        logging.info("Close %s receiver %s at %s", self.data_type, self.name, self.receive_address)
 
     def setup_plots(self, parent):
         raise NotImplementedError("You have to implement a setup_plots method!")
 
+    def handle_data_if_active(self, data):
+        ''' Forwards the data to the data handling function of the reveiver is active'''
+        if self._active:
+            self.handle_data(data)
+
     def handle_data(self, data):
-        ''' Handle data gets a dictionary with data and sets the visualization accordningly'''
+        ''' Handle data gets a dictionary with data and sets the visualization accordningly. It is only called if the receiver is active.'''
         raise NotImplementedError("You have to implement a handle_data method!")
 
     def deserialze_data(self, data):
