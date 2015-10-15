2eec9030ee19a6f8655ec3cbdb497daa2dc4fff1
DavidLP <pohl@physik.uni-bonn.de>
2015-10-15 20:03:36 +0200

    MAINT

--- examples/full_example/main.py
+++ examples/full_example/main.py
@@ -1,5 +1,4 @@
 import subprocess
-import time
 import psutil
 import os
 import sys
@@ -13,10 +12,6 @@ def run_script_in_shell(script, arguments):
     return subprocess.Popen("python %s %s" % (script, arguments), shell=True, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP if os.name == 'nt' else 0)
 
 
-def run_script_in_process(script, arguments):
-    return subprocess.Popen(["python", script, arguments], shell=True if os.name == 'nt' else False, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP if os.name == 'nt' else 0)
-
-
 def kill(proc):  # kill process by id, including subprocesses; works for linux and windows
     process = psutil.Process(proc.pid)
     for child_proc in process.children(recursive=True):
@@ -25,16 +20,17 @@ def kill(proc):  # kill process by id, including subprocesses; works for linux a
 
 if __name__ == '__main__':
     # Start the simulation producer to create some fake data
-    producer_process = run_script_in_process(r'../../online_monitor/utils/producer_sim.py', 'configuration.yaml')
+    producer_process = run_script_in_shell(r'../../online_monitor/utils/producer_sim.py', 'configuration.yaml')
 
     # Start the converter
     settings.add_converter_path(r'examples/converter')  # needed to find the converter
     settings.add_receiver_path(r'examples/receiver')  # needed to find the receiver
-    converter_manager_process = run_script_in_process(r'../../online_monitor/start_converter.py', r'configuration.yaml')
+    converter_manager_process = run_script_in_shell(r'../../online_monitor/start_converter.py', r'configuration.yaml')
 
     # Helper function to run code after OnlineMonitor Application exit
     def appExec():
         app.exec_()
+        # Stop other processes
         kill(converter_manager_process)
         kill(producer_process)
 
@@ -43,7 +39,3 @@ if __name__ == '__main__':
     win = OnlineMonitorApplication(r'configuration.yaml')
     win.show()
     sys.exit(appExec())
-# #     time.sleep(5)
-# 
-
-#     print('\n')
