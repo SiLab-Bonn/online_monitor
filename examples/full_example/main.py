3976d4a50ba9dabeb2a9818b5fda4f71e80d6121
DavidLP <pohl@physik.uni-bonn.de>
2015-10-27 13:11:09 +0100

    MAINT

--- examples/full_example/main.py
+++ examples/full_example/main.py
@@ -3,9 +3,9 @@ import os
 
 from online_monitor.utils import settings
 
+
 def run_script_in_shell(script, arguments, command=None):
     return subprocess.Popen("%s %s %s" % ('python' if not command else command, script, arguments), shell=True, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP if os.name == 'nt' else 0)
-
 
 if __name__ == '__main__':
     settings.add_converter_path(r'examples/converter')  # needed to find the converter
