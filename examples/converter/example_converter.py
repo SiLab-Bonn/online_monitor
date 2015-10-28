83231e84bbd24fd7d5c3080d7a9b48ffa3376c74
DavidLP <pohl@physik.uni-bonn.de>
2015-10-28 10:24:53 +0100

    ENH: add name to data description to be able to distinguish producers

--- examples/converter/example_converter.py
+++ examples/converter/example_converter.py
@@ -8,7 +8,7 @@ class ExampleConverter(Transceiver):
     def interpret_data(self, data):  # apply a threshold to the data
         data_interpreted = json.loads(data[0], object_hook=utils.json_numpy_obj_hook)['position'].copy()
         data_interpreted[data_interpreted < self.config['threshold']] = 0
-        return {'position_with_threshold': data_interpreted}
+        return {'position_with_threshold_%s' % self.name: data_interpreted}
 
     def serialze_data(self, data):
         return [json.dumps(data, cls=utils.NumpyEncoder)]
