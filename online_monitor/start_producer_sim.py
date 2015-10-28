6e00328e11d1759f2ae7e187af0e56669a8ca620
DavidLP <pohl@physik.uni-bonn.de>
2015-10-28 10:29:10 +0100

    ENH: new producer simulation manager start script

---
+++ online_monitor/start_producer_sim.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from online_monitor.utils import utils
+from online_monitor.converter.producer_sim_manager import ProducerSimManager
+
+
+def main():
+    args = utils.parse_arguments()
+    utils.setup_logging(args.log)
+
+    cm = ProducerSimManager(args.config_file)
+    cm.start()  # blocking function, returns on SIGTERM signal
+
+if __name__ == '__main__':
+    main()
