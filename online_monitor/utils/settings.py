b5b4717a887ae1d90597ddfa2fd81dfe635b230e
DavidLP <pohl@physik.uni-bonn.de>
2015-10-28 10:27:53 +0100

    ENH: add producer simulation

--- online_monitor/utils/settings.py
+++ online_monitor/utils/settings.py
@@ -32,6 +32,17 @@ def add_receiver_path(path):  # path where to search for converter modules
         config.write(f)
 
 
+def add_producer_sim_path(path):  # path where to search for converter modules
+    config = ConfigParser.SafeConfigParser()
+    config.read(_file_name)
+    paths = get_producer_sim_path()
+    paths.append(path)  # append actual path
+    paths = list(set(paths))  # remove duplicates
+    config.set('producer_sim', 'path', str(paths)[1:-1])  # store new string representation
+    with open(_file_name, 'w') as f:
+        config.write(f)
+
+
 def delete_converter_path(path):  # path where to search for converter modules
     config = ConfigParser.SafeConfigParser()
     config.read(_file_name)
@@ -50,6 +61,15 @@ def delete_receiver_path(path):  # path where to search for converter modules
         config.write(f)
 
 
+def delete_producer_sim_path(path):  # path where to search for converter modules
+    config = ConfigParser.SafeConfigParser()
+    config.read(_file_name)
+    paths = [p for p in get_producer_sim_path() if p != path]
+    config.set('producer_sim', 'path', str(paths)[1:-1])  # store new string representation
+    with open(_file_name, 'w') as f:
+        config.write(f)
+
+
 def get_converter_path():
     config = ConfigParser.SafeConfigParser()
     config.read(_file_name)  # WARNING: ConfigParser.NoSectionError: can mean no file at all!
@@ -63,6 +83,15 @@ def get_receiver_path():
     config = ConfigParser.SafeConfigParser()
     config.read(_file_name)  # WARNING: ConfigParser.NoSectionError: can mean no file at all!
     path = ast.literal_eval(config.get('receiver', 'path'))
+    if isinstance(path, tuple):
+        return [p for p in path]
+    return [path]
+
+
+def get_producer_sim_path():
+    config = ConfigParser.SafeConfigParser()
+    config.read(_file_name)  # WARNING: ConfigParser.NoSectionError: can mean no file at all!
+    path = ast.literal_eval(config.get('producer_sim', 'path'))
     if isinstance(path, tuple):
         return [p for p in path]
     return [path]
