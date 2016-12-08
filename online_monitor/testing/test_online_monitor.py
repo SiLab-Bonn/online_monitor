74aee17890fa8433c068ffaa98c5097ca9aebce1
DavidLP <david-leon.pohl@rub.de>
2016-12-08 16:49:10 +0100

    REG: use Qt5 properly, #11

--- online_monitor/testing/test_online_monitor.py
+++ online_monitor/testing/test_online_monitor.py
@@ -8,7 +8,7 @@ import subprocess
 import time
 import os
 import psutil
-from PyQt5.QtGui import QApplication
+from PyQt5.QtWidgets import QApplication
 
 import online_monitor
 from online_monitor import OnlineMonitor
