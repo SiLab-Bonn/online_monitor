871a9b47d7a6e0200fbe88b4fa6cb191e8c60483
owtscharenko <owtscharenko@physik.uni-bonn.de>
2018-05-23 14:16:00 +0200

    BUG: find instance by cmd line

--- online_monitor/stop_online_monitor.py
+++ online_monitor/stop_online_monitor.py
@@ -4,7 +4,7 @@ import psutil
 
 def main():
     for proc in psutil.process_iter():
-        if any(name in proc.name() for name in ['start_producer', 'start_converter', 'start_online']):
+        if any(name in proc.name() for name in ['start_producer', 'start_converter', 'start_online']) or any(name in ''.join(proc.cmdline()) for name in ['start_producer', 'start_converter', 'start_online']):
             proc.kill()
 
 
