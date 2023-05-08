b2981f05ce7cfdc8392e2b72594aeb8d1f031598
leloup314 <wolf@physik.uni-bonn.de>
2023-05-08 17:04:37 +0200

    ENH: rework transceiver to be more robust wrt to thread-safety

--- online_monitor/converter/transceiver.py
+++ online_monitor/converter/transceiver.py
@@ -58,6 +58,8 @@ class Transceiver(multiprocessing.Process):
         self.frontend_socket_type = zmq.SUB
         # Std. setting is unidirectional backend communication
         self.backend_socket_type = zmq.PUB
+        # Whether the transceiver is bidirectional
+        self._is_bidirectional = False
 
         if 'max_cpu_load' in kwarg:
             logging.warning('The parameter max_cpu_load is deprecated! Use max_buffer!')
@@ -90,12 +92,12 @@ class Transceiver(multiprocessing.Process):
         logging.info('Set bidirectional communication for converter %s '
                      'backend', self.name)
         self.backend_socket_type = zmq.DEALER
+        self._is_bidirectional = True
 
     def _setup_frontend(self):
         ''' Receiver sockets facing clients (DAQ systems)
         '''
         self.frontends = []
-        self.fe_poller = zmq.Poller()
         for actual_frontend_address in self.frontend_address:
             # Subscriber or server socket
             actual_frontend = (actual_frontend_address,
@@ -106,31 +108,24 @@ class Transceiver(multiprocessing.Process):
             actual_frontend[1].set_hwm(10)
             # A suscriber has to set to not filter any data
             if self.frontend_socket_type == zmq.SUB:
-                actual_frontend[1].setsockopt_string(zmq.SUBSCRIBE, u'')
+                actual_frontend[1].setsockopt(zmq.SUBSCRIBE, b'')
             actual_frontend[1].connect(actual_frontend_address)
             self.frontends.append(actual_frontend)
-            self.fe_poller.register(actual_frontend[1], zmq.POLLIN)
-        self.raw_data = Queue()
-        self.fe_stop = threading.Event()
-
+        
     def _setup_backend(self):
         ''' Send sockets facing services (e.g. online monitor, other forwarders)
         '''
         self.backends = []
-        self.be_poller = zmq.Poller()
         for actual_backend_address in self.backend_address:
             # publisher or client socket
             actual_backend = (actual_backend_address,
                               self.context.socket(self.backend_socket_type))
             # Wait 0.5 s before termating socket
             actual_backend[1].setsockopt(zmq.LINGER, 500)
-            # Buffer only 100 meassages, then throw data away
+            # Buffer only 10 meassages, then throw data away
             actual_backend[1].set_hwm(10)
             actual_backend[1].bind(actual_backend_address)
             self.backends.append(actual_backend)
-            if self.backend_socket_type != zmq.DEALER:
-                self.be_poller.register(actual_backend[1], zmq.POLLIN)
-        self.be_stop = threading.Event()
 
     def _setup_transceiver(self):
         # ignore SIGTERM; signal shutdown() is used for controlled proc. term.
@@ -138,43 +133,50 @@ class Transceiver(multiprocessing.Process):
         # Setup ZeroMQ connections, has to be within run;
         # otherwise ZMQ does not work
         self.context = zmq.Context()
+        
+        # Front end data queue and event
+        self.raw_data = Queue()
+        self.fe_stop = threading.Event()
 
-        self._setup_frontend()
+        # If we have a bidirectional socket, e want to recv commnads
+        if self._is_bidirectional:
+            self.cmds = Queue()
+            self.cmd_stop = threading.Event()
+
         self._setup_backend()
 
     def recv_data(self):
+        
+        # Sockets should be created within the thread that accesses them
+        self._setup_frontend()
+
         while not self.fe_stop.is_set():
-            self.fe_poller.poll(1)  # max block 1 ms
             raw_data = []
             # Loop over all frontends
             for actual_frontend in self.frontends:
-                try:
+                if actual_frontend[1].poll(timeout=1, flags=zmq.POLLIN):
                     actual_raw_data = actual_frontend[1].recv(flags=zmq.NOBLOCK)
                     raw_data.append((actual_frontend[0],
                                      self.deserialize_data(actual_raw_data)))
-                except zmq.Again:  # no data
-                    pass
             if raw_data:
                 self.raw_data.put_nowait(raw_data)
 
     def recv_commands(self):
-        if self.backend_socket_type == zmq.DEALER:
-            while not self.be_stop.wait(0.1):
-                self.be_poller.poll(1)  # max block 1 ms
-                commands = []
-                # Check for bidirectional communication
-                if self.backend_socket_type == zmq.DEALER:
-                    for actual_backend in self.backends:
-                        try:
-                            # Check if command was send
-                            command = actual_backend[1].recv_json(zmq.NOBLOCK)
-                            logging.debug("%s converter %s received command %s",
-                                          self.kind, self.name, command)
-                            commands.append(command)
-                        except zmq.error.Again:
-                            pass
-                if commands:
-                    self.handle_command(commands)
+        while not self.cmd_stop.wait(0.1):
+            if not self.cmds.empty():
+                self.handle_command(self.cmds.get_nowait())
+
+    def check_for_cmds(self):
+        cmds = []
+        for actual_backend in self.backends:
+            if actual_backend[1].poll(timeout=1, flags=zmq.POLLIN):
+                command = actual_backend[1].recv_json(zmq.NOBLOCK)
+                logging.debug("%s converter %s received command %s",
+                                self.kind, self.name, command)
+                cmds.append(command)
+        if cmds:
+            self.cmds.put_nowait(cmds)
+                    
 
     def send_data(self, data):
         ''' This function can be overwritten in derived class
@@ -194,39 +196,53 @@ class Transceiver(multiprocessing.Process):
         process = psutil.Process(self.ident)  # access this process info
         self.cpu_load = 0.
 
-        be_thread = threading.Thread(target=self.recv_commands)
-        be_thread.start()
+        # Spawn thread for receiving data
         fe_thread = threading.Thread(target=self.recv_data)
         fe_thread.start()
+
+        # Spawn thread for receiving commands if needed
+        if self._is_bidirectional:
+            cmd_thread = threading.Thread(target=self.recv_commands)
+            cmd_thread.start()
 
         logging.debug("Start %s transceiver %s at %s", self.kind, self.name,
                       self.backend_address)
         while not self.exit.wait(0.01):
-            if self.raw_data.empty():
-                continue
-            else:
+            
+            # Checks for commands; if commands are received, they are worked sequentially within a separate thread
+            self.check_for_cmds()
+            
+            # Check for data
+            if not self.raw_data.empty():
+                
                 raw_data = self.raw_data.get_nowait()
 
-            actual_cpu_load = process.cpu_percent()
-            # Filter cpu load by running mean since it changes rapidly;
-            # cpu load spikes can be filtered away since data queues up
-            # through ZMQ
-            self.cpu_load = 0.90 * self.cpu_load + 0.1 * actual_cpu_load
-            # Check if already too many messages queued up then omit data
-            if not self.max_buffer or self.max_buffer > self.raw_data.qsize():
-                data = self.interpret_data(raw_data)
-                # Data is None if the data cannot be converted
-                # (e.g. is incomplete, broken, etc.)
-                if data is not None and len(data) != 0:
-                    self.send_data(data)
-            else:
-                logging.warning(f'Converter {self.name} cannot keep up, omitting data for interpretation!')
+                actual_cpu_load = process.cpu_percent()
+                # Filter cpu load by running mean since it changes rapidly;
+                # cpu load spikes can be filtered away since data queues up
+                # through ZMQ
+                self.cpu_load = 0.90 * self.cpu_load + 0.1 * actual_cpu_load
+                # Check if already too many messages queued up then omit data
+                if not self.max_buffer or self.max_buffer > self.raw_data.qsize():
+                    data = self.interpret_data(raw_data)
+                    # Data is None if the data cannot be converted
+                    # (e.g. is incomplete, broken, etc.)
+                    if data is not None and len(data) != 0:
+                        self.send_data(data)
+                    else:
+                        logging.warning(f"Converter {self.name} data after interpretation is None or empty!")
+                else:
+                    logging.warning(f'Converter {self.name} cannot keep up, omitting data for interpretation!')
 
-        self.be_stop.set()
-        be_thread.join()
+        # Set all threads stop signals and block until they finish
         self.fe_stop.set()
         fe_thread.join()
-        # Close connections
+
+        if self._is_bidirectional:
+            self.cmd_stop.set()
+            cmd_thread.join()
+
+        # Close all sockets from main thread and terminate the context
         for actual_frontend in self.frontends:
             actual_frontend[1].close()
         for actual_backend in self.backends:
